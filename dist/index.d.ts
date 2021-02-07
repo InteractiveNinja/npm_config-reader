dba91222c6e2349d231d2863428c95561d6ca99a
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-02-07 19:15:30 +0100

    added d.ts file and changed test

---
+++ dist/index.d.ts
@@ -0,0 +1,31 @@
+/**
+ * creates a Config Manager
+ * @param path Path to Config File
+ */
+export declare let config: (path: string) => ConfigManager;
+declare class ConfigManager {
+    private path;
+    private json;
+    /**
+     * Creates a Config Manager
+     * @param path Path of the Config File
+     */
+    constructor(path: string);
+    /**
+     * gets the Value out of the Config File with the correct key
+     */
+    get(key: string): any;
+    /**
+     * Sets or Adds an Config Paramater
+     */
+    set(key: string, val: any): void;
+    /**
+     * Reads the Config File and returns JSON Array
+     */
+    private readFile;
+    /**
+     * writes into the Config file
+     */
+    private writeFile;
+}
+export {};
--- test/indexApp.js
+++ test/indexApp.js
@@ -4,27 +4,26 @@ const app = require('../dist/index');
 
 describe('Config Value Read Int', () => {
     it('config sollte mit key "port" = "211" zurück gegeben', () => {
-        let config = app.config("config.json")
+        let config = app.config("./test/config.json")
         assert.strictEqual(config.get("port"), 211)
     })
 })
 describe('Config Value Read String', () => {
-    it('config sollte mit key "title" = "Hello World" zurück gegeben', () => {
-        let config = app.config("config.json")
+    it('config sollte mit key "name" = "Hello World" zurück gegeben', () => {
+        let config = app.config("./test/config.json")
         assert.strictEqual(config.get("name"), "Hello World")
     })
 })
 describe('Config Value Read boolean', () => {
     it('config sollte mit key "calling" = "true" zurück gegeben', () => {
-        let config = app.config("config.json")
+        let config = app.config("./test/config.json")
         assert.strictEqual(config.get("calling"), true)
     })
 })
 
 describe('Config Value doesnt Exit', () => {
     it('config sollte mit key "gerge" = "undefined" zurück gegeben', () => {
-        let config = app.config("config.json")
-        console.log(config.get("gerge"))
+        let config = app.config("./test/config.json")
         assert.strictEqual(config.get("gerge"), undefined)
     })
 })
